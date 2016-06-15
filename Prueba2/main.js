cc83fad2893bcf44073c528ae125d48283209a90
anderseliz <andy_eliz.sali@hotmail.com>
2016-06-14 20:46:48 -0500

    Problema2

---
+++ Prueba2/main.js
@@ -0,0 +1,14 @@
+var arr1 = [1,2,3,4,5];
+var arr2 = [];
+function clonaArreglo(source){
+	var arr = [];
+	source.forEach(function(e){
+		arr.push(e);
+	})
+	return arr;
+}
+
+var arr2 = clonaArreglo(arr1);
+
+console.log(arr1);
+console.log(arr2);
