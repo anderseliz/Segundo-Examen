dbda17665314fa1b92367ab7b34d53e98c3f7366
anderseliz <andy_eliz.sali@hotmail.com>
2016-06-14 20:47:13 -0500

    Problema3

---
+++ Prueba3/main.js
@@ -0,0 +1,15 @@
+function Telefono(numero,marca,color){
+	this.numero = numero;
+	this.marca = marca;
+	this.color = color;
+	this.llamadas = 0;
+	this.marcar = function(numero){
+		console.log(this.numero + " le marca a " + numero);
+		this.llamadas++;
+	}
+}
+
+var tel1 = new Telefono(81069632,"Telmex","Rojo");
+var tel2 = new Telefono(83003065,"Telcel","Azul");
+
+tel1.marcar(tel2.numero);
